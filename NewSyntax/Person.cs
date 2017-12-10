c96c78d452d6c1f503efdcba8499b3dcd40b7648
Runner <lbrunner@163.com>
2017-12-11 07:32:14 +0800

    完成了前七个

---
+++ NewSyntax/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewSyntax
+{
+    public class Person
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public int Age { get; set; }
+    }
+}
---
+++ NewSyntax/Program.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewSyntax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            C06AnmMethod();
+
+            Console.ReadKey();
+        }
+
+        #region 1.0 自动属性
+
+        #endregion
+
+        #region 2.0 var的用法
+        static void C02Var()
+        {
+            var person = new Person();
+        }
+        #endregion
+
+        #region 3.0 匿名类
+        /// <summary>
+        /// 小结：
+        /// 1、匿名类的写法 new { key = value的形式定义你想要的属性}
+        /// 2、匿名类本质上被编译器生成了一个泛型类，在此类的构造函数中给相应的字符赋值
+        /// 3、当两个匿名类的参数名称一样，个数一样，但是类型不一样 共用一个泛型类
+        /// 4、当两个匿名类的个数不一样或者参数名称不一样必须重新生成各自的泛型类
+        /// 5、注意 匿名类必须要赋初始值
+        /// </summary>
+        static void C03AnmClass()
+        {
+            //正常类的写法
+            Person person = new Person();
+
+            //匿名类的写法
+            var dog = new { Name = "小花", Age = 1, ToyName = "皮球", };
+
+            var dog1 = new { Name = 1, Age = 1, ToyName = "皮球", };
+
+            var dog2 = new { Name = 1, Age = 1, };
+
+            var dog3 = new { Age = 1, Name = 1, };
+
+            var dog4 = new { Name111 = 1, Age = 1, };
+
+            Console.WriteLine(dog.Name + "," + dog.Age + "," + dog.ToyName);
+        }
+        #endregion
+
+        #region 4.0 参数默认值 和 命名参数
+        static void C04DefaultValue()
+        {
+            C0401("boy", 22);
+            C0401(); //编译器会自动根据C0401方法中的参数默认值补全此处的写法：C0401("tom",22);
+            C0401("joy");
+            C0401(age: 20);
+        }
+
+        static void C0401(string name = "tom", int age = 22)
+        {
+            Person person = new Person();
+
+            person.Age = age;
+            person.Name = name;
+        }
+        #endregion
+
+        #region 5.0 对象、集合 初始化器
+        static void C05ObjectAndArrayInit()
+        {
+            //对象
+            Person person = new Person();
+
+            person.Name = "job";
+
+            Person p = new Person()
+            {
+                Name = "123",
+                Age = 20
+            };
+
+            //集合
+            var list = new List<Person>
+            {
+                new Person(){ Name = "123",Age = 20},
+                new Person(){ Name = "123",Age = 20},
+            };
+
+            //数组
+            Person[] perArr = new Person[4];
+            perArr[0] = new Person();
+
+            Person[] perArr02 = new Person[]
+            {
+                new Person(),
+                new Person(),
+                new Person(),
+            };
+
+        }
+        #endregion
+
+        #region 6.0 匿名方法（匿名函数、匿名委托） 和 Lambad表达式的推断
+        //定义了一个整数入参，返回类型为bool的委托
+        public delegate bool DeleTest(int num);
+
+        //定义一个和委托具有相同签名的方法
+        static bool Prcss(int num)
+        {
+            return num > 3;
+        }
+
+        static void C06AnmMethod()
+        {
+            //1.0 匿名方法的写法规则 delegate(参数类型 参数名称){方法体代码}
+
+            //2.0 利用和委托Predicate<T>具有相同签名的方法进行查询
+            var list = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            //需求：从list中找出大于3的所有元素，以新集合返回
+            var nlist = list.FindAll(Prcss);
+            foreach (var item in nlist)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("--------------------------------");
+
+            //3.0 利用匿名委托实现上面的需求
+            var n2list = list.FindAll(delegate (int num) { return num > 3; });
+
+            foreach (var item in n2list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        /// <summary>
+        /// 需求：从Person对象集合中查找出年龄大于20，并且名称中包含m的对象，以新集合返回
+        /// </summary>
+        static void C0601AnnTest()
+        {
+            //1.0 定义集合
+            var list = new List<Person>()
+            {
+                new Person(){ Name = "tom", Age = 23},
+                new Person(){ Name = "boy", Age =18},
+                new Person(){ Name = "toc", Age = 29}
+            };
+
+            //2.0 利用FindAll方法实现 
+            //2.0.1 定义自己的方法与Predcate<T>具有相同签名的方法where进行查询
+            var nlist = list.FindAll(where);
+            foreach (var item in nlist)
+            {
+                Console.WriteLine(item);
+            }
+
+            //2.0.2 利用匿名函数进行查询
+            var nlist2 = list.FindAll(delegate (Person p) { return p.Age > 20 && p.Name.Contains("m"); });
+            foreach (var item in nlist2)
+            {
+                Console.WriteLine(item);
+            }
+
+            //2.0.3 lambda表达式的推断
+            //第一步演变
+            // => goes to
+            var nlist3 = list.FindAll((Person p) => { return p.Age > 20 && p.Name.Contains("m"); });
+
+            //第二步演变
+            var nlist4 = list.FindAll(p => p.Age > 20 && p.Name.Contains("m"));
+
+            /*
+             * 总结：
+             *  1、如果参数只有一个，则可以省略小括号，只留下形参名称，如果参数数量大于一个，则必须保留小括号，例如(person,car) => true
+             *  2、当方法体只有一句代码的时候，可以省略大括号和最后的分号，如果有返回值的可以省略return关键字
+             *  3、当方法体有多句代码的时候，不可以省略大括号，如果有返回值的也不能省略return
+             */
+
+            var nlist5 = list.FindAll(p =>
+            {
+                p.Age = p.Age + 10;
+                return p.Age > 20 && p.Name.Contains("m");
+            });
+        }
+
+        static bool where(Person p)
+        {
+            return p.Age > 20 && p.Name.Contains("m");
+        }
+        #endregion
+
+        #region 7.0 扩展方法
+        /// <summary>
+        /// 扩展方法的要素：
+        ///1、此方法必须是一个静态方法
+        ///2、此方法必须放在静态类中
+        ///3、此方法的第一个参数必须以this开头，并且指定此方法是扩展自哪个类型上的
+        ///下面告诉特点和本质
+        ///4、扩展方法扩展自哪个类型，就必须是此类型的变量来点,其他类型无法使用
+        ///5、扩展方法中的this后面的参数不属于 方法的参数
+        ///6、如果扩展方法和实例方法具有相同的签名，则优先调用实例方法
+        ///7、扩展自父类上的方法，可以被子类的对象直接使用
+        ///8、扩展自接口上的扩展方法，可以被实现类的对象直接使用
+        ///9、扩展方法的本质，最终还是被编译器编译成了 静态类.静态方法()
+        /// </summary>
+        static void C07ExtMethod()
+        {
+            DateTime now = DateTime.Now;
+            string fmtStr1 = now.ToString("yyyy-MM-dd HH:mm:ss");
+            string fmtStr2 = now.ToString("yyyy-MM-dd");
+            //使用一般方法
+            string fmtStr3 = ExtHelper.FmtDate(now);
+
+            //使用扩展方法
+            //此种写法是 语法糖 最终被编译成了 ExtHelper.FmtDate(now)
+            string fmtStr4 = now.FmtDate();
+        }
+        #endregion
+    }
+
+    public static class ExtHelper
+    {
+        /// <summary>
+        /// 扩展方法的要素：
+        /// 1、此方法必须是一个静态方法
+        /// 2、此方法必须放在静态类中
+        /// 3、此方法的第一个参数必须以this开头，并且指定此方法是扩展自哪个类型上的
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <returns></returns>
+        public static string FmtDate(this DateTime dt)
+        {
+            return dt.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+
+        //public static string FmtDate(DateTime dt)
+        //{
+        //    return dt.ToString("yyyy-MM-dd HH:mm:ss");
+        //}
+    }
+}
