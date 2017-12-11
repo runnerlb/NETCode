c1201c482ed4b3bbdfeb5ffdf7520b5a5da8e560
Runner <lbrunner@163.com>
2017-12-11 23:22:50 +0800

    完成了新语法模块的准备工作

--- NewSyntax/Person.cs
+++ NewSyntax/Person.cs
@@ -8,6 +8,7 @@ namespace NewSyntax
 {
     public class Person
     {
+        public int Id { get; set; }
         private string _name;
 
         public string Name
@@ -17,5 +18,17 @@ namespace NewSyntax
         }
 
         public int Age { get; set; }
+
+        public int TypeId { get; set; }
+        public override string ToString()
+        {
+            return string.Format("Id:{2},Name:{0}, Age:{1}, TypeId:{3}", this.Name, this.Age, this.Id, this.TypeId);
+        }
+    }
+
+    public class WorkType
+    {
+        public int TypeId { get; set; }
+        public string TypeName { get; set; }
     }
 }
--- NewSyntax/Program.cs
+++ NewSyntax/Program.cs
@@ -10,10 +10,36 @@ namespace NewSyntax
     {
         static void Main(string[] args)
         {
-            C06AnmMethod();
+            C1003GroupBy();
 
             Console.ReadKey();
         }
+
+        #region 0.0 帮助集合
+        static List<Person> GetPersonList()
+        {
+            return new List<Person>()
+            {
+                new Person(){Id=4, Name ="张三",Age = 12,TypeId=1},
+                new Person(){Id=6, Name ="李四",Age = 100,TypeId=3},
+                new Person(){Id=5, Name ="王五",Age = 12,TypeId=2},
+                new Person(){Id=3, Name ="赵六",Age = 34,TypeId=1},
+                new Person(){Id=2, Name ="宋七",Age = 200,TypeId=4},
+                new Person(){Id=1, Name ="周八",Age = 1,TypeId=1},
+            };
+        }
+
+        static List<WorkType> GetWorkTypeList()
+        {
+            return new List<WorkType>()
+            {
+                new WorkType(){ TypeId=1, TypeName="程序员"},
+                new WorkType(){ TypeId=2, TypeName="公务员"},
+                new WorkType(){ TypeId=3, TypeName="教师"},
+                new WorkType(){ TypeId=4, TypeName="司机"}
+            };
+        }
+        #endregion
 
         #region 1.0 自动属性
 
@@ -224,6 +250,275 @@ namespace NewSyntax
             //此种写法是 语法糖 最终被编译成了 ExtHelper.FmtDate(now)
             string fmtStr4 = now.FmtDate();
         }
+        #endregion
+
+        #region 8.0 系统内置委托
+
+        #region 8.0.1 Action 接受参数 无返回值
+        static void C0801Action()
+        {
+            var list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            //需求 输出集合中的元素
+
+            //使用最原始的方法
+            //list.ForEach(C0801Method);
+
+            //使用匿名委托
+            //list.ForEach(delegate (int i) { Console.WriteLine(i); });
+
+            //使用labmada表达式
+            //list.ForEach(i => Console.WriteLine(i));
+        }
+
+        static void C0801Method(int i)
+        {
+            Console.WriteLine(i);
+        }
+
+        #endregion
+
+        #region 8.0.2 Predicate 接受一个参数， 返回一个bool值
+        static void C0802Predicate()
+        {
+            var list = GetPersonList();
+
+            var nlist = list.FindAll(p => p.Age > 20);
+
+            nlist.ForEach(c => Console.WriteLine(c.Age));
+
+            var person = list.Find(p => p.Age > 20);
+
+            Console.WriteLine(person.Age);
+        }
+        #endregion
+
+        #region 8.0.3 Func 接受参数 返回参数，但是不固定
+        static void C0803Func()
+        {
+            var list = GetPersonList();
+            //需求：查找list中年龄大于30的所有人的集合
+            var nlist = list.Where(p => p.Age > 30).ToList();
+            var nlist2 = list.FindAll(p => p.Age > 30);
+
+            nlist.ForEach(c => Console.WriteLine(c.ToString()));
+            Console.WriteLine("------------------------");
+            nlist2.ForEach(c => Console.WriteLine(c.ToString()));
+        }
+        #endregion
+
+        #region 8.0.4 Comparison 返回一个整数 接受两个同类型的参数
+        static void C0804Comparison()
+        {
+            var list = GetPersonList();
+
+            //需求:将list中的所有对象按照年龄倒序排列
+            //list.Sort(C0804Sort);
+
+            //list.Sort(delegate (Person p1, Person p2) { return p1.Age - p2.Age; });
+
+            list.Sort((p1, p2) => p1.Age - p2.Age);
+
+            list.ForEach(p => Console.WriteLine(p.ToString()));
+        }
+
+
+        static int C0804Sort(Person p1, Person p2)
+        {
+            return p1.Age - p2.Age;
+        }
+        #endregion
+        #endregion
+
+        #region 9.0 SQO方法 - 标准查询运算符
+
+        #region 9.0.1 Where() FirstOrDefult() LastOrDefault方法进行查找操作
+        static void C0901()
+        {
+            var list = GetPersonList();
+
+            //利用where查找出list中年龄大于5 或者名称等于小猪的
+            //链式编程
+            list.Where(p => p.Age > 12 || p.Name.Contains("王")).ToList().ForEach(p => Console.WriteLine(p.ToString()));
+
+            Console.WriteLine("---------------------------");
+
+            //FirstOrDefault
+            var person = list.FirstOrDefault(p => p.Age > 12 || p.Name.Contains("王"));
+            Console.WriteLine(person.ToString());
+
+            Console.WriteLine("---------------------------");
+
+            var person1 = list.LastOrDefault(p => p.Age > 12 || p.Name.Contains("王"));
+            Console.WriteLine(person1.ToString());
+        }
+        #endregion
+
+        #region 9.0.2 对集合进行排序(正序和倒叙)
+        static void C0902OrderBy()
+        {
+            var list = GetPersonList();
+
+            list.OrderBy(c => c.Age).ToList().ForEach(c => Console.WriteLine(c.ToString()));
+
+            Console.WriteLine("----------------------------");
+
+            list.OrderByDescending(c => c.Age).ToList().ForEach(c => Console.WriteLine(c.ToString()));
+
+        }
+        #endregion
+
+        #region 9.0.3 对集合进行多个字段排序
+        /*
+         * 总结：
+         * 1、对一个集合进行单个字段的升序排列使用：OrderBy  降序:OrderByDescending
+         * 2、对一个集合进行多个字段排列：的一个字段使用OrderBy  或OrderByDescending之一
+         *      第2个字段或者以后的字段则使用ThenBy和ThenByDescending之一
+         */
+        static void C0903ThenBy()
+        {
+            var list = GetPersonList();
+            //需求，先根据Age倒序后再根据id倒序
+            //Sql语句：select * from list order by Age desc,ID desc
+
+            //list.OrderBy(p => p.Age).OrderBy(p => p.Id).ToList().ForEach(p => Console.WriteLine(p.ToString()));
+
+            list.OrderBy(p => p.Age).ThenByDescending(p => p.Id).ToList().ForEach(p => Console.WriteLine(p.ToString()));
+        }
+        #endregion
+
+        #region 9.0.4 投影方法 Select
+        static void C0904Select()
+        {
+            var list = GetPersonList();
+            //需求1：将list集合中的Name属性的值获取成一个新的集合
+            List<string> nlist = list.Select(p => p.Name).ToList();
+            nlist.ForEach(s => Console.WriteLine(s));
+
+            //需求2：将list集合中的 Name和Age属性获取成一个新的对象的集合
+
+            var nlist2 = list.Select(p => new { p.Name, p.Age }).ToList();
+
+            nlist2.ForEach(p => Console.WriteLine(p.ToString()));
+        }
+
+        #endregion
+
+        #region 9.0.5 分页方法 Skip Take
+        static void C0905SkipTake()
+        {
+            var list = GetPersonList();
+
+            //跳过第一行 获取两行
+            var nlist = list.Skip(1).Take(2).ToList();
+            nlist.ForEach(p => Console.WriteLine(p.ToString()));
+
+            Console.WriteLine("___________");
+
+            int total;
+            var nlist2 = C0905Page(3, 2, out total);
+            nlist2.ForEach(p => Console.WriteLine(p.ToString()));
+        }
+
+        static List<Person> C0905Page(int pageIndex, int pageSize, out int Total)
+        {
+            var list = GetPersonList();
+
+            Total = list.Count;
+
+            return list.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList();
+        }
+        #endregion
+
+        #region 9.0.6 Join联表操作
+        static void C0906Join()
+        {
+            var plist = GetPersonList();
+            var wlist = GetWorkTypeList();
+
+            //需求：查找piglist集合中的Name和ID值同时和typelist中的TName的值作为一个新的匿名类集合返回
+            //SQL: select p.Name,p.ID,t.TName from piglist p join typelist t on (p.TypeID=t.TypeID)
+
+            //var nlist = plist.Join(wlist, p => p.TypeId, w => w.TypeId, (p, w) => new { p.Name, p.Age, p.Id, w.TypeId, w.TypeName }).ToList();
+
+            var nlist = plist.Join(wlist, p => p.TypeId, w => w.TypeId, (p, w) => new { p.Name, p.Age, w.TypeId, w.TypeName }).ToList();
+            nlist.ForEach(m => Console.WriteLine(m.ToString()));
+
+        }
+        #endregion
+
+        #region 9.0.7 GroupBy分组
+        static void C0907GroupBy()
+        {
+            var list = GetPersonList();
+
+            list.GroupBy(p => p.TypeId).ToList().ForEach(c =>
+            {
+                Console.WriteLine(c.Key);
+                c.ToList().ForEach(g => Console.WriteLine(g.ToString()));
+            });
+
+        }
+        #endregion
+        #endregion
+
+        #region 10.0 Linq语法，本质上还是会拆分成SQO方法进行调用
+
+        #region 10.1 用Linq实现Where
+        static void C1001Where()
+        {
+            var list = GetPersonList();
+
+            var nlist = (from p in list
+                         where p.Age > 20
+                         select p).ToList();
+
+            nlist.ForEach(p => Console.WriteLine(p.ToString()));
+        }
+        #endregion
+
+        #region 10.0.2 用Linq实现排序
+        static void C1002Sort()
+        {
+            var list = GetPersonList();
+
+            var nlist = (from p in list
+                         orderby p.Age descending, p.Id ascending
+                         select p).ToList();
+
+            nlist.ForEach(p => Console.WriteLine(p.ToString()));
+        }
+        #endregion
+
+        #region 10.0.3 用Linq实现集合的链接
+        static void C1003Join()
+        {
+            var plist = GetPersonList();
+            var wlist = GetWorkTypeList();
+
+            var nlist = from p in plist
+                        join w in wlist on p.TypeId equals w.TypeId
+                        select new { p.Name, p.Age, p.TypeId, w.TypeName };
+
+            nlist.ToList().ForEach(c => Console.WriteLine(c.ToString()));
+        }
+        #endregion
+
+        #region 10.0.4 用Linq实现分组
+        static void C1003GroupBy()
+        {
+            var plist = GetPersonList();
+
+            var nlist = from p in plist
+                        group p by p.TypeId;
+
+            nlist.ToList().ForEach(c =>
+            {
+                Console.WriteLine(c.Key);
+                c.ToList().ForEach(p => Console.WriteLine(p.ToString()));
+            });
+        }
+        #endregion
         #endregion
     }
 
